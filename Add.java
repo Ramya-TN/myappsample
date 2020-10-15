c88cabdf8893869fdcbacd1c0235c2baa7095a4f
Ramya-TN <55545715+Ramya-TN@users.noreply.github.com>
2020-10-15 11:13:36 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,22 @@
+class Add
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		int a=5,b=10,sum=0;
+		System.out.println("value of a is" + a);
+		System.out.println("value of a is" + b);
+		System.out.println("value of a is" + sum);
+		sum=a+b;
+		System.out.println(sum);
+		
+		String s1="Ramya";
+		String s2=s1+a;
+		//String s3=s1+s2;
+		System.out.println(s1);
+		System.out.println(s2);
+		//System.out.println(s3);
+		//System.out.println(s1+ " \t " +s2);
+		System.out.println(s2);
+	}
+}	
---
+++ Assignment.java
@@ -0,0 +1,14 @@
+class Assignment
+{
+	public static void main(String args[])
+	{
+		int l=10,b=5,area=0;
+		area=l*b;
+		//System.out.println(area);
+		String s1="I have finished my assignment.";
+		//System.out.println(s1);
+		//System.out.println("Area of Rectangle is" +area);
+		String s2="My name is Ravi";
+		System.out.println(s1 + "Area of Rectangle is" + area + s2);
+	}
+}
